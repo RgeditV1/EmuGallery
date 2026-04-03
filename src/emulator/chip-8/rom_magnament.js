e77c6dd02398b5b63ce0d929403ccdb19ce50c92
Rgeditv1 <rgeditv1@protonmail.com>
2026-04-03 01:18:15 -0400

    implementacion del cpu listo

---
+++ src/emulator/chip-8/rom_magnament.js
@@ -0,0 +1,6 @@
+const roms = [
+    { name: "Pong", file: "../emulator/chip-8/roms/pong.ch8", desc: "El clásico de 1972." },
+    { name: "Space Invaders", file: "../emulator/chip-8/roms/invaders.ch8", desc: "Destruye a los aliens." }
+];
+
+console.log(roms);
